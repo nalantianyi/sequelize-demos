cb5f80e001c3e20eb1ebea772b23254d8da3ac39
纳兰天忆 <103520146@qq.com>
2017-05-16 14:02:44 +0800

    connection model

--- chapter1/model.js
+++ chapter1/model.js
@@ -3,3 +3,20 @@
  */
 
 const {Sequelize,sequelize}=require('./connection');
+const {STRING}=Sequelize;
+
+const User=sequelize.define('user',{
+    firstName:{
+        type:STRING,
+        field:'first_name'
+    },
+    lastName:{
+        type:STRING
+    }
+},{freezeTableName:true});
+User.sync({force:true}).then(()=>{
+    return User.create({
+        firstName:'John',
+        lastName:'Hancock'
+    });
+});
